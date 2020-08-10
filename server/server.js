bbc5c5dcfd55ede170a783fe3e93180860a318ad
DiegoTrenquin1 <diegoeduardotc@hotmail.com>
2020-08-10 15:43:21 -0400

    Cambio de agregar linea de router usuario

--- server/server.js
+++ server/server.js
@@ -14,6 +14,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 
+app.use(require('./routes/usuario'));
 
 mongoose.connect('mongodb://localhost:27017/cafe', (err, res) => {
     if (err) throw err;
