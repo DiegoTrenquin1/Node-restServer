7194a92ac7ea48b4bdfd1e0a404b8503e31d77d3
DiegoTrenquin1 <diegoeduardotc@hotmail.com>
2020-08-10 15:32:27 -0400

    Npm start listo y partes del otro

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 require('./config/config.js');
 
 const express = require('express');
+const mongoose = require('mongoose');
+
+
 const bodyParser = require('body-parser');
 const app = express()
 
@@ -10,36 +13,16 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-app.get('/usuario', function(req, res) {
-    res.json('get Usuario')
-})
-app.post('/usuario', function(req, res) {
-
-    let body = req.body;
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-    } else {
-        res.json({
-            persona: body
-        });
-    }
 
 
-})
-app.put('/usuario/:id', function(req, res) {
+mongoose.connect('mongodb://localhost:27017/cafe', (err, res) => {
+    if (err) throw err;
 
-    let id = req.params.id;
+    console.log('Base de datos online');
+});
 
-    res.json({
-        id
-    });
-})
-app.delete('/usuario', function(req, res) {
-    res.json('delete Usuario')
-})
+
+
 app.listen(3000, () => {
     console.log("Escuchando el puerto 3000", process.env.PORT);
 })
