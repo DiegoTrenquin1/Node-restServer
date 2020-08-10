6f57becf3d856a9f1aab4be92d7bf8fd529a5e59
DiegoTrenquin1 <diegoeduardotc@hotmail.com>
2020-08-10 15:48:39 -0400

    Npm start listo y partes del otro

--- server/server.js
+++ server/server.js
@@ -5,13 +5,13 @@ const mongoose = require('mongoose');
 
 
 const bodyParser = require('body-parser');
-const app = express()
+const app = express();
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 
 app.use(require('./routes/usuario'));
@@ -26,4 +26,4 @@ mongoose.connect('mongodb://localhost:27017/cafe', (err, res) => {
 
 app.listen(3000, () => {
     console.log("Escuchando el puerto 3000", process.env.PORT);
-})
+});
